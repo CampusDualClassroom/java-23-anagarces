390da3e1b9c2cd8e7b87634d26b677be6b1ced80
Ana Garces <anagarcestech@gmail.com>
2025-01-10 15:22:30 +0100

    ejercicio 23 resuelto

--- src/main/java/com/campusdual/classroom/Exercise23.java
+++ src/main/java/com/campusdual/classroom/Exercise23.java
@@ -6,15 +6,44 @@ import java.util.Map;
 public class Exercise23 {
 
     public static Map<String, Person> createHashMap() {
+        //creamos una coleccion de tipo map inicializada como hashmap
+        Map<String, Person> customMap = new HashMap<>();
 
+        //anadimos las entradas
+        customMap.put("person", new Person("John", "Smith"));
+        customMap.put("teacher", new Teacher("María", "Montessori", "Educación"));
+        customMap.put("police", new PoliceOfficer("Jake", "Peralta", "B-99"));
+        customMap.put("doctor", new Doctor("Gregory", "House", "Nefrología e infectología"));
+
+        return customMap;
     }
 
     public static Person addMapValue(Map<String, Person> map, String key, Person value) {
 
+            map.put(key, value);
+                System.out.println("Nuevo valor añadido.");
+             return value;
     }
+
 
     public static void printMapValues(Map<String, Person> map) {
+        //muestra los valores por consola
+        map.forEach((clave, valor)->{
+            valor.getDetails();
+        });
 
     }
-    
+
+    public static void main(String[] args) {
+
+    //creamos una nueva coleccion de mapa llamando al metodo correspondiente
+    Map<String, Person> customMap = createHashMap();
+
+    //nuevo elemento para el mapa
+     addMapValue(customMap, "police", new PoliceOfficer("Charles", "Boyle", "B-99"));
+
+    //mostramos por consola los valores
+    printMapValues(customMap);
+
+    }
 }
